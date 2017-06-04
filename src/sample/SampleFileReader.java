80996ec389f5b40b9dd9d8c8270f9678162be4d8
karthikyits <karthik@yitsol.com>
2017-06-04 23:55:41 +0530

    Create SampleFileReader.java

---
+++ src/sample/SampleFileReader.java
@@ -0,0 +1,36 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SampleFileReader {
+	public static void main(String[] args)  {
+		File file = new File("d:/temp/test.text");
+
+			FileReader fr;
+			try {
+				fr = new FileReader(file);
+				BufferedReader br =	
+						new	
+						BufferedReader(fr);
+				String line;
+				while((line = br.readLine()) != null){
+					System.out.println(line);
+				}
+				br      
+				.
+				close()
+				;
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+					
+	}
+}
