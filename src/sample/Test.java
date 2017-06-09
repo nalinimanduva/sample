d48c98e04bf90dd24147bdeb8b6f56b34b4b34e6
NALINIMANDUVA <nalini.manduva@gmail.com>
2017-06-09 12:13:30 -0400

    First commit through code

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -7,30 +7,37 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class SampleFileReader {
-	public static void main(String[] args)  {
-		File file = new File("d:/temp/test.text");
 
-			FileReader fr;
-			try {
-				fr = new FileReader(file);
-				BufferedReader br =	
-						new	
-						BufferedReader(fr);
-				String line;
-				while((line = br.readLine()) != null){
-					System.out.println(line);
-				}
-				br      
-				.
-				close()
-				;
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-					
-	}
+    public static void main(String[] args) {
+        File file = new File("d:/temp/test.text");
+        FileReader fr;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            _vf_safeClose(br);
+        }
+    }
+
+    public static void _vf_safeClose(BufferedReader arg0) {
+        if (arg0 != null) {
+            try {
+                arg0.close();
+            } catch (IOException e) {
+            // TODO VioFixer: handle your logging here
+            }
+        }
+    }
 }
