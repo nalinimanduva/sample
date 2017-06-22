1a6d854120f422cd360acb03098a24a47b808521
nalinimanduva <nalini@yitsol.com>
2017-06-22 14:11:19 -0400

    Create SampleNullDefer.java

---
+++ src/sample/SampleNullDefer.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SampleNullDefer {
+
+    public static void main(String[] args) {
+  
+    }
+
+}
